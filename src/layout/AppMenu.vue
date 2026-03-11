3e0e9c35fd774635c6d58a9d1614ed761c2f9050
rick_chen <rick_chen@asgard.com.tw>
2026-03-11 15:14:08 +0800

    init

---
+++ src/layout/AppMenu.vue
@@ -0,0 +1,22 @@
+<script setup>
+import { ref } from 'vue';
+import AppMenuItem from './AppMenuItem.vue';
+
+const model = ref([
+    {
+        label: '首頁',
+        items: [{ label: '控制台', icon: 'pi pi-fw pi-home', to: '/' }]
+    }
+]);
+</script>
+
+<template>
+    <ul class="layout-menu">
+        <template v-for="(item, i) in model" :key="item">
+            <app-menu-item v-if="!item.separator" :item="item" :index="i"></app-menu-item>
+            <li v-if="item.separator" class="menu-separator"></li>
+        </template>
+    </ul>
+</template>
+
+<style lang="scss" scoped></style>
---
+++ src/router/index.js
@@ -0,0 +1,21 @@
+import AppLayout from '@/layout/AppLayout.vue';
+import { createRouter, createWebHistory } from 'vue-router';
+
+const router = createRouter({
+    history: createWebHistory(),
+    routes: [
+        {
+            path: '/',
+            component: AppLayout,
+            children: [
+                {
+                    path: '/',
+                    name: 'dashboard',
+                    component: () => import('@/views/Dashboard.vue') // 留下這一個就好
+                }
+            ]
+        }
+    ]
+});
+
+export default router;
